2594801492290a3225592c3e8008f4d1afb1ac2d
rohitsengar001 <rohitsengar001@gmail.com>
2022-01-25 18:02:11 +0530

    define the handlers in task controllers

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Task } from './task.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private tasksService: TasksService) { }
+
+    //define the handlers
+    @Get()
+    getAllTask(): Task[] {
+        return this.tasksService.getAllTask();
+    }
+    @Post()
+    createTask(@Body('title') title, @Body('description') description): Task {
+        return this.tasksService.createTask(title, description);
+    }
+}
