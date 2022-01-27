c54f3592e62d0b654372f2ebedb949918e25297a
rohitsengar001 <rohitsengar001@gmail.com>
2022-01-27 15:58:01 +0530

    Revert "install pkg typeorm for postgres"
    This reverts commit c31a8cd637b3ef7c86f03b2f9dcb9d9a6aee243e.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [
-    TasksModule,
-    TypeOrmModule.forRoot({
-      type:'postgres',
-      host:'locahost',
-      port:5432,
-      username:'postgres',
-      password:'postgres',
-      database:'task-management',
-      autoLoadEntities:true,
-      synchronize:true
-    })
-  ],
+  imports: [TasksModule],
   controllers: [],
   providers: [],
 })
