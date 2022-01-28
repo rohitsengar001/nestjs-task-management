1e125738cd8498aa7dff1312cff3138eda2df4a4
rohitsengar001 <rohitsengar001@gmail.com>
2022-01-28 17:32:12 +0530

    password hashing : signup user

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,23 +1,34 @@
-import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
-
+import * as bcrypt from 'bcrypt';
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  //create user: signup
   async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialDto;
 
-    const user = this.create({ username, password });
+    //start hashing password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log('salt', salt);
+    console.log('password', hashedPassword);
+    //end hasing password
+    //create user
+    const user = this.create({ username, password: hashedPassword });
     try {
-      await this.save(user);
+      await this.save(user); //save users into dbms
     } catch (error) {
-        console.log(error.code);
-        if(error.code == '23505'){
-            throw new ConflictException('Username already exists');
-        }else{
-            throw new InternalServerErrorException();
-        }
+      console.log(error.code);
+      if (error.code == '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
     }
   }
 }
