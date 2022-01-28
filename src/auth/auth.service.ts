962f4d8438b6f806eb1b1ea076f74613ee9b66a5
rohitsengar001 <rohitsengar001@gmail.com>
2022-01-28 17:52:42 +0530

    signin api done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
   ) {}
 
-  async signUp(authCredentialDto: AuthCredentialsDto):Promise<void>{
-      return this.userRepository.createUser(authCredentialDto)
+  async signUp(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialDto);
+  }
+
+  async signIn(authCredentialDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialDto;
+    const user = await this.userRepository.findOne({ username }); //find data for primary key: username
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Please check your login credential');
+    }
   }
 }
