d669c5e986ac7be5b6c2771c2aef775141a0029f
rohitsengar001 <rohitsengar001@gmail.com>
2022-01-30 03:38:04 +0530

    Tasks & Users : Database Relation

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,13 +1,23 @@
-import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { TaskEntity } from 'src/tasks/task.entity';
+import {
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
-export class User{
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({unique: true})
-    username: string;
+  @Column({ unique: true })
+  username: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
+
+  @OneToMany((type) => TaskEntity, (task) => task.user, { eager: true })
+  tasks: TaskEntity[];
 }
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,17 +1,21 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { TaskStatus } from "./task-status-enum";
+import { User } from 'src/auth/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatus } from './task-status-enum';
 
 @Entity()
-export class TaskEntity{
-    @PrimaryGeneratedColumn('uuid')
-    id:string;
+export class TaskEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    title:string
+  @Column()
+  title: string;
 
-    @Column()
-    description: string
-    
-    @Column()
-    status: TaskStatus
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+
+  @ManyToOne((type) => User, (user) => user.tasks, { eager: false })
+  user: User;
 }
